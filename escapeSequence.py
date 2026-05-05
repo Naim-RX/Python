2a0e032ad84fb8563e9e1116ff58df929dcb79a9
Naim-RX <naimasraful2@gmail.com>
2026-05-05 16:40:01 +0600

    Escape Sequence Characters Added

---
+++ escapeSequence.py
@@ -0,0 +1,17 @@
+# This prints "Hi" and then moves to a new line (\n),
+# then prints "My Name is Naim" on the next line
+print("Hi \n My Name is Naim")
+
+# This prints: Hi  My Name Is "Naim"
+# The \" is used to include double quotes inside the string
+print("Hi  My Name Is \"Naim\"")
+
+
+# Print statement with custom separator and end
+# sep="~" means values will be separated by "~"
+# end="999" means after printing, it will NOT go to a new line,
+# instead it will end with "999"
+print("Hey", 6, sep="~", end="999")
+
+# This will continue printing on the same line because previous print didn't add a newline
+print("Naim")
