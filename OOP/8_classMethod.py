0572a9e49c7bebf9728703df22b4fa4e9eb6639e
Naim-RX <naimasraful2@gmail.com>
2026-07-22 19:36:07 +0600

    Class method as alternative constructor Added

--- OOP/8_classMethod.py
+++ OOP/8_classMethod.py
@@ -2,8 +2,9 @@ class Student:
 
     university = "DIU"
 
-    def __init__(self, name):
+    def __init__(self, name,id):
         self.name = name
+        self.id = id
 
     # Instance method
     def show_name(self):
@@ -14,18 +15,29 @@ class Student:
     def show_university(cls):
         print(cls.university)
 
+    #Class method as alternative constructor
+    @classmethod
+    def fromstr(cls,string):
+        return cls(string.split("-")[0],string.split("-")[1])
+
     # Static method
     @staticmethod
     def greet():
         print("Welcome to Python")
 
-s = Student("Naim")
+s = Student("Naim",101)
 
 # Instance method
 s.show_name()
 
 # Class method
 Student.show_university()
+
+#Class method as alternative constructor
+str = "Naim-101"
+s1 = Student.fromstr(str)
+print(s1.name)
+print(s1.id)
 
 # Static method
 Student.greet()
